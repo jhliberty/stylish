694f8fccd4d07fb07b31c12776a0a84c38382e09
Jonathan Soeder <jonathan.soeder@gmail.com>
2014-09-12 01:38:51 -0500

    additional specs

--- spec/acceptance/listing_assets_spec.rb
+++ spec/acceptance/listing_assets_spec.rb
@@ -4,12 +4,15 @@ describe "Browsing Assets", :type => :request do
   it "should display info about an individual asset" do
     get "/stylish/meta/app/assets/javascripts/test.coffee"
     expect(json["digest"]).not_to be_empty
-    expect(json["content"][/require_self/,0]).not_to be_empty
+  end
+
+  it "should display content from an individual asset" do
+    get "/stylish/content/app/assets/javascripts/test.coffee"
+    expect(response.body).to match(/require_self/)
   end
 
   it "should list the assets under a path" do
     get "/stylish/list/app/assets/javascripts"
-
     expect(json["path"]).to eq("app/assets/javascripts")
     expect(json["is_folder"]).to be_truthy
     expect(json["children"]).not_to be_empty
